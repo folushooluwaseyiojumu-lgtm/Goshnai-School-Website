9c4fcff39b961a6ab42d66239aa859b395f2d38f
Coded funds -predictions <folushooluwaseyiojumu@gmail.com>
2026-08-16 12:27:15 +0100

    Create admin-login.js

---
+++ admin-login.js
@@ -0,0 +1,30 @@
+function loginAdmin() {
+
+    const username =
+        document.getElementById("username").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    if (
+        username === "admin" &&
+        password === "12345"
+    ) {
+
+        localStorage.setItem(
+            "adminLoggedIn",
+            "true"
+        );
+
+        window.location.href =
+            "admin-dashboard.html";
+
+    } else {
+
+        alert(
+            "Invalid login details."
+        );
+
+    }
+
+}
